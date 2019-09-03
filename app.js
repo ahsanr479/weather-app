1008956b83708608e2bfe8eafd3ecd7039404793
Ahsan-Rahim <Ahsan.Rahim@ibm.com>
2019-09-03 10:29:52 -0500

    added express to the app can now be run on the browser

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
+const express = require('express');
 
-const address = process.argv[2];
 
-if(!address){
-    console.log('please provide an address');
-}
-else{
-    geocode(address,(error,{latitude, longitude,location}) =>{
+const app = express();
+
+app.get('/weather',(req,res) => {
+    if(!req.query.address){
+        res.send('Please enter address')
+    }
+    geocode(req.query.address,(error,{latitude,longitude,location}) => {
         if(error){
-            return console.log(error);
+            res.send(error)
         }
         forecast(latitude,longitude,(error,weatherData) => {
             if(error){
-                return console.log(error);
+                res.send(error)
             }
-            console.log(location);
-            console.log(weatherData);
+            res.send(weatherData.forecast)
         })
-        
     })
-}
+})
+
+app.listen(3000,() => {
+    console.log('server is up on port 3000')
+})
+
+
 
 
--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitde,longitude,callback) => {
             callback('Unable to find location',undefined);
         }else{
             callback(undefined,{
-                forecast:`${body.daily.data[0].summary} It is  currently ${body.currently.temperature} degrees out.There is a ${body.currently.precipProbability}% chance of rain`
+                forecast:`${body.daily.data[0].summary} It is  currently ${body.currently.temperature} degrees out.There is a ${body.daily.data[0].precipProbability}% chance of rain`
             })
         }
     })
